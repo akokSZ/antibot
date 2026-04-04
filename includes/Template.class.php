72ff429a1ced34b6fe218f5318ed1a30e40cf63d
Egor NiKO <niko_egor@mail.ru>
2026-04-04 17:03:10 +0300

    Доб. animals в подстказки

--- includes/Template.class.php
+++ includes/Template.class.php
@@ -20,7 +20,7 @@ class Template
         $this->Logger = $logger;
 
         $this->Config->init('main', 'header404', false, 'отдает на заглушку 404 заголовок');        
-        $this->skin_captcha = $this->Config->init('main', 'captcha_type', $this->skin_captcha, 'Тип капчи: checkbox, slider, slider_rotate, slider_zsay');
+        $this->skin_captcha = $this->Config->init('main', 'captcha_type', $this->skin_captcha, 'Тип капчи: checkbox, slider, slider_rotate, slider_zsay, animals');
         $this->utm_referrer = $this->Config->init('main', 'utm_referrer', $this->utm_referrer, 'вкл/выкл');
         $this->save_referer = $this->Config->init('main', 'save_referer', $this->save_referer, 'вкл/выкл сохраненние referer в localStorage');
 
